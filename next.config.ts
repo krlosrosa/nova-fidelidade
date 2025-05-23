36af0d84f775485cd36c53f8e5cb2cb0365e6672
carlos <robertorsilva.krlos@gmail.com>
2025-05-23 15:10:13 -0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-    env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
+  output: 'standalone',
 };
 
 export default nextConfig;
